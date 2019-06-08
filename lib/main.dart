420b60b13cb83920fc90756a54c5ca84f2598d73
marcioquimbundo <marcioquimbundo@hotmail.com>
2019-06-08 22:31:48 +0100

    camera preview added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:tik_tok_clone_flutter/src/ui/home.dart';
 
-void main() => runApp(MyApp());
+List<CameraDescription> cameras;
+
+Future<Null> main() async {
+  try {
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    //logError(e.code, e.description);
+  }
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
@@ -12,12 +23,11 @@ class MyApp extends StatelessWidget {
       title: "TikTokUI",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        textTheme: Theme.of(context).textTheme.apply(
-          bodyColor: Colors.white,
-          displayColor: Colors.white,
-        )
-      ),
-      home: Home(),
+          textTheme: Theme.of(context).textTheme.apply(
+                bodyColor: Colors.white,
+                displayColor: Colors.white,
+              )),
+      home: Home(cameras),
     );
   }
 }
--- lib/src/ui/home.dart
+++ lib/src/ui/home.dart
@@ -2,8 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:tik_tok_clone_flutter/src/ui/widgets/actions_toolbar.dart';
 import 'package:tik_tok_clone_flutter/src/ui/widgets/bottom_toolbar.dart';
 import 'package:tik_tok_clone_flutter/src/ui/widgets/video_description.dart';
+import 'package:camera/camera.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  final List<CameraDescription> cameras;
+
+  Home(this.cameras);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  CameraController controller;
+  @override
+  void initState() {
+    super.initState();
+    try {
+      onCameraSelected(widget.cameras[0]);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
   Widget get topSection => Container(
         height: 100.0,
         child: Row(
@@ -30,9 +57,53 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final deviceRation = size.width / size.height;
+    if (!controller.value.initialized) {
+      return new Center(
+        child: CircularProgressIndicator(),
+      );
+    }
     return Scaffold(
         backgroundColor: Colors.black,
-        body: Column(
-            children: <Widget>[topSection, middleSection, BottomToolBar()]));
+        body: Center(
+          child: Stack(
+            children: <Widget>[
+              (!controller.value.initialized)
+                  ? Container()
+                  : CameraPreview(
+                      controller,
+                    ),
+              Column(children: <Widget>[
+                topSection,
+                middleSection,
+                BottomToolBar(),
+                SizedBox(
+                  height: 10,
+                )
+              ]),
+            ],
+          ),
+        ));
+  }
+
+  void onCameraSelected(CameraDescription cameraDescription) async {
+    if (controller != null) await controller.dispose();
+    controller = CameraController(cameraDescription, ResolutionPreset.medium);
+
+    controller.addListener(() {
+      if (mounted) setState(() {});
+      if (controller.value.hasError) {
+        print('Camera Error: ${controller.value.errorDescription}');
+      }
+    });
+
+    try {
+      await controller.initialize();
+    } on CameraException catch (e) {
+      print(e);
+    }
+
+    if (mounted) setState(() {});
   }
 }
